2488842e95e174ecb7ef90f0b17320736a81fa7c
Aubrey Taylor <aubrey@hall.ly>
2025-03-14 23:13:54 -0600

    fix database file

--- backend/api.mjs
+++ backend/api.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import database from "./database";
+import database from "./database.mjs";
 
 const router = express.Router();
 
