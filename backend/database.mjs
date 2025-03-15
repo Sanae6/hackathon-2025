41f25ca1ea133754962ce1064e37e9d7ecc234c8
Aubrey Taylor <aubrey@hall.ly>
2025-03-15 00:13:28 -0600

    added basic api endpoint and database code

--- backend/database.mjs
+++ backend/database.mjs
@@ -1,7 +1,34 @@
+import {writeFileSync, readFileSync, existsSync} from "fs";
+
+const databaseFilename = "database.json";
+/**
+ * @type {DatabaseSchema}
+ */
 const database = {
   tutors: [],
   users: [],
+  bookings: [],
 };
 
+if (existsSync(databaseFilename))
+  database = readFileSync(databaseFilename, "utf-8");
+
+function save() {
+  writeFileSync(databaseFilename, JSON.stringify(database), "utf-8");
+}
+
+/**
+ * @param {number} id
+ * @returns {Tutor}
+ */
+export function getTutor(id) {
+  return database.tutors.find(tutor => tutor.tutorID === id);
+}
+
+export function updateTutorPronouns(id, pronouns) {
+  const tutor = database.tutors.find(tutor => tutor.tutorID === id);
+  if (tutor !== undefined)
+    tutor.pronouns = pronouns;
+}
 
 export default undefined;
